3cc6a855c3f871a039df18a81de6e61e06ddd0cf
Giri Kuncoro <giri.kuncoro@go-jek.com>
2020-12-13 14:44:39 +0700

    Init kudemo app with vue-cli
    Signed-off-by: Giri Kuncoro <giri.kuncoro@go-jek.com>

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Kubernetes logo" src="./assets/kubernetes.png" />
+    <Hello msg="Halo! Selamat Datang di KuDemo App" />
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+
+export default {
+  name: "App",
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Untuk Kubernetes Demo yang mudah diikuti.</p>
+    <h3>Tautan Penting</h3>
+    <ul>
+      <li>
+        <a href="https://kubernetes.io/id" target="_blank" rel="noopener"
+          >Dokumentasi Kubernetes dalam Bahasa Indonesia</a
+        >
+      </li>
+      <br />
+      <li>
+        <a
+          href="https://t.me/kubernetesindonesia"
+          target="_blank"
+          rel="noopener"
+          >Telegram Komunitas Kubernetes Indonesia</a
+        >
+      </li>
+      <br />
+      <li>
+        <a href="https://cloudnative.id" target="_blank" rel="noopener"
+          >Talks tentang Cloud Native dan Kubernetes di Indonesia</a
+        >
+      </li>
+    </ul>
+    <h3>Penulis</h3>
+    <ul>
+      <li>
+        <a href="https://twitter.com/girikuncoro" target="_blank" rel="noopener"
+          >Giri Kuncoro</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/" target="_blank" rel="noopener"
+          >Aris Risdianto</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
