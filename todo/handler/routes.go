e5b44005ddb6cf3cfdfe5cd664c08b9cfcc13288
Giri Kuncoro <giri.kuncoro@go-jek.com>
2020-12-16 19:51:43 +0700

    Implement service handler for TODO crud

--- todo/handler/routes.go
+++ todo/handler/routes.go
@@ -12,6 +12,18 @@ func InitRoutes() *http.ServeMux {
 	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/static", todoHandler.GetStatic)
+	mux.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			todoHandler.getAllTodo(w, r)
+		case http.MethodPost:
+			todoHandler.insertTodo(w, r)
+		case http.MethodDelete:
+			todoHandler.deleteTodo(w, r)
+		default:
+			responseError(w, http.StatusNotFound, "")
+		}
+	})
 
 	return mux
 }
--- todo/handler/todo.go
+++ todo/handler/todo.go
@@ -2,14 +2,17 @@ package handler
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/ngoprek-kubernetes/kudemo/todo/db"
+	"github.com/ngoprek-kubernetes/kudemo/todo/schema"
 	"github.com/ngoprek-kubernetes/kudemo/todo/service"
 )
 
 type todoHandler struct {
-	static *db.Static
+	postgres *db.Postgres
+	static   *db.Static
 }
 
 func (h *todoHandler) GetStatic(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +24,66 @@ func (h *todoHandler) GetStatic(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	responseOK(w, todoList)
+}
+
+func (h *todoHandler) getAllTodo(w http.ResponseWriter, r *http.Request) {
+	ctx := db.SetRepo(r.Context(), h.postgres)
+
+	todoList, err := service.GetAll(ctx)
+	if err != nil {
+		responseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	responseOK(w, todoList)
+}
+
+func (h *todoHandler) insertTodo(w http.ResponseWriter, r *http.Request) {
+	ctx := db.SetRepo(r.Context(), h.postgres)
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	var todo schema.Todo
+	if err := json.Unmarshal(b, &todo); err != nil {
+		responseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := service.Insert(ctx, &todo)
+	if err != nil {
+		responseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	responseOK(w, id)
+}
+
+func (h *todoHandler) deleteTodo(w http.ResponseWriter, r *http.Request) {
+	ctx := db.SetRepo(r.Context(), h.postgres)
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	var req struct {
+		ID int `json:"id"`
+	}
+	if err := json.Unmarshal(b, &req); err != nil {
+		responseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := service.Delete(ctx, req.ID); err != nil {
+		responseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func responseOK(w http.ResponseWriter, body interface{}) {
