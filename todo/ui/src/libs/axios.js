0703221673e131723fd16371f28271f8eee77689
Giri Kuncoro <giri.kuncoro@go-jek.com>
2021-03-03 20:38:42 +0700

    Change baseURL production to /api/todo
    Signed-off-by: Giri Kuncoro <giri.kuncoro@go-jek.com>

--- todo/ui/src/libs/axios.js
+++ todo/ui/src/libs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     // For dev running from docker-compose
-    // baseURL: "http://localhost:8080",
-    // For prod talking to API in Kubernetes
-    baseURL: "http://todo-api:8080",
+    // baseURL: "http://localhost:8080/todo",
+    // For prod talking to API in Kubernetes via Ingress
+    baseURL: "/api/todo",
 });
