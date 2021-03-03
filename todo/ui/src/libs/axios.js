54da8286685ba0c7834c105ae375c9ae2521caf0
Giri Kuncoro <giri.kuncoro@go-jek.com>
2021-03-03 20:43:22 +0700

    Remove suffix of /todo when calling API
    Signed-off-by: Giri Kuncoro <giri.kuncoro@go-jek.com>

--- todo/ui/src/libs/axios.js
+++ todo/ui/src/libs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     // For dev running from docker-compose
-    // baseURL: "http://localhost:8080/todo",
+    // baseURL: "http://localhost:8080",
     // For prod talking to API in Kubernetes via Ingress
-    baseURL: "/api/todo",
+    baseURL: "/api",
 });
